d687493e90961958338137d5bde1da0cde6c985d
darkosavic <darkosavic1997@gmail.com>
2020-04-03 16:11:08 +0200

    add tag model, seeder, migration

---
+++ database/seeds/TagsSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class TagsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        \DB::table('tags')->truncate();
+
+        $categories = ["Technology", "Fashion", "Sports", "Economy", "Business"];
+
+        foreach ($categories as $category) {
+            \DB::table('tags')->insert([
+                // Generates a random department name
+                'name' => $category,
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s'),
+            ]);
+        }
+    }
+}
