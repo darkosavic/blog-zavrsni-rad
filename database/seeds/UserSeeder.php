7d0fc993fc9416cd78fe4196bf7ae308dde0f610
darkosavic <darkosavic1997@gmail.com>
2020-04-09 19:03:20 +0200

    add me as user

--- database/seeds/UserSeeder.php
+++ database/seeds/UserSeeder.php
@@ -18,9 +18,17 @@ class UserSeeder extends Seeder {
             'updated_at' => date('Y-m-d H:i:s'),
         ]);
 
+//        \DB::table('users')->insert([
+//            'name' => 'Pera Peric',
+//            'email' => 'pera.peric@cubes.rs',
+//            'password' => \Hash::make('cubesphp'),
+//            'created_at' => date('Y-m-d H:i:s'),
+//            'updated_at' => date('Y-m-d H:i:s'),
+//        ]);
+        
         \DB::table('users')->insert([
-            'name' => 'Pera Peric',
-            'email' => 'pera.peric@cubes.rs',
+            'name' => 'Darko Savic',
+            'email' => 'darkosavic1997@gmail.com',
             'password' => \Hash::make('cubesphp'),
             'created_at' => date('Y-m-d H:i:s'),
             'updated_at' => date('Y-m-d H:i:s'),
